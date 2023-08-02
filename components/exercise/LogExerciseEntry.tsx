7308708126bb925b788e33ca9f8572300da2919e
Tyler Lundin <TylerJamesLundin@gmail.com>
2023-08-02 04:42:54 -0700

    UI/UX updates

--- components/exercise/LogExerciseEntry.tsx
+++ components/exercise/LogExerciseEntry.tsx
@@ -7,65 +7,68 @@ import RepsStep from './steps/RepsStep';
 import WeightStep from './steps/WeightStep';
 import IntensityStep from './steps/IntensityStep';
 import FailureStep from './steps/FailureStep';
-import NotesStep from './steps/NotesStep';
-import TagsStep from './steps/TagsStep';
 import ExerciseOverviewStep from './steps/ExerciseOverviewStep';
 import StepControls from '../StepControls';
-import { nextExerciseStep, prevExerciseStep } from '@/_store/slices/exerciseSlice';
+import { nextExerciseStep, prevExerciseStep, resetNewExercise, setExerciseStep } from '@/_store/slices/exerciseSlice';
 import logExerciseThunk from '@/_store/thunks/logExerciseThunk';
 import Breadcrumbs, { Breadcrumb } from '../Breadcrumbs';
+import { toggleAddExercise } from '@/_store/slices/uiSlice';
+import { Button } from '../ui/button';
+import { useRouter } from 'next/navigation';
 
 
 export default function LogExerciseEntry() {
   const { currentStep, exercise: { name }, weight, reps, intensity, toFailure, notes, tags } = useSelector((state: RootState) => state.exercise.newEntry)
   const dispatch = useDispatch<AppDispatch>();
+  const router = useRouter();
   const STEPS = [
-    <ListExercisesStep key={`exercise-${name}-list`} />,
-    <WeightStep key={`${name}-weight`} />,
-    <RepsStep key={`${name}-reps`} />,
-    <IntensityStep key={`${name}-intensity`} />,
-    <FailureStep key={`${name}-failure`} />,
-    <NotesStep key={`${name}-notes`} />,
-    <TagsStep key={`${name}-tags`} />,
-    <ExerciseOverviewStep key={`${name}-overview`} />,
+    { name: "ListExercisesStep", component: <ListExercisesStep /> },
+    { name: "WeightStep", component: <WeightStep /> },
+    { name: "RepsStep", component: <RepsStep /> },
+    { name: "IntensityStep", component: <IntensityStep /> },
+    { name: "FailureStep", component: <FailureStep /> },
+    { name: "ExerciseOverviewStep", component: <ExerciseOverviewStep /> },
   ]
 
   const isLastStep = currentStep === STEPS.length - 1;
   const isFirstStep = currentStep === 0;
+  const isReady = name && weight && reps && intensity;
   const nextStep = () => dispatch(nextExerciseStep());
   const prevStep = () => dispatch(prevExerciseStep());
-  const handleClose = () => console.log('go back');
-  const handleSubmit = () => dispatch(logExerciseThunk());
+  const handleClose = () => {
+    dispatch(resetNewExercise())
+    router.push("/dashboard/exercise");
+    const handleSubmit = () => dispatch(logExerciseThunk());
 
-  const stepControlsProps = {
-    isLastStep,
-    isFirstStep,
-    nextStep,
-    prevStep,
-    handleClose,
-    handleSubmit,
-    className: "flex fixed w-full h-fit bottom-4 left-0 items-center justify-between z-20 p-2 pointer-events-none"
-  }
+    const stepControlsProps = {
+      isLastStep,
+      isFirstStep,
+      nextStep,
+      prevStep,
+      handleClose,
+      className: "flex w-full h-fit absolute items-center justify-between z-20 pointer-events-none top-0 px-8 top-1/2 -translate-y-1/2"
+    }
 
-  const breadcrumbs: Breadcrumb[] = [
-    { title: name },
-    { title: weight ? weight.toString() + ' lbs' : '0 lbs' },
-    { title: reps ? reps.toString() + ' reps' : '0 reps' },
-    { title: intensity ? intensity.toString() + ' RPE' : '0 RPE' },
-    { title: toFailure ? 'to failure' : 'not to failure' },
-    { title: notes ? notes.substring(0, 10) : 'notes' },
-    { title: tags.length ? `${tags.length} tags` : '0 tags' },
-  ].slice(0, currentStep + 1);
+    const breadcrumbs: Breadcrumb[] = [
+      { title: name ? name : 'exercise', onClick: () => dispatch(setExerciseStep(0)) },
+      { title: weight ? `${weight.toString()} lbs` : 'weight?', onClick: () => dispatch(setExerciseStep(1)) },
+      { title: reps ? `${reps.toString()} reps` : 'reps?', onClick: () => dispatch(setExerciseStep(2)) },
+      { title: intensity ? `${intensity.toString()} RPE` : 'intensity?', onClick: () => dispatch(setExerciseStep(3)) },
+      { title: toFailure ? 'to failure' : 'failed?', onClick: () => dispatch(setExerciseStep(4)) },
+      { title: isReady ? "ready" : "overview", onClick: () => dispatch(setExerciseStep(5)) }
+    ];
 
-  return (
-    <div className=" z-50 h-screen w-screen overflow-y-auto bg-white dark:bg-black absolute left-0 top-16">
-      <Breadcrumbs breadcrumbs={breadcrumbs} />
-      <StepControls {...stepControlsProps} />
-      <div className="absolute top-20 left-0 w-screen h-full px-2">
-        {STEPS[currentStep]}
-        <div className="h-20"></div>
+    return (
+      <div className="h-[calc(100vh_-_7rem)]">
+        <div className="fixed w-full top-20 z-50 dark:bg-black bg-white">
+          <Breadcrumbs breadcrumbs={breadcrumbs} currentStep={currentStep} />
+          <div className="grid place-content-center">
+            <StepControls {...stepControlsProps} />
+            <Button disabled={!isReady} variant={!isReady ? "glassRed" : "glassGreen"} onClick={handleSubmit}>Log Exercise</Button>
+          </div>
+        </div>
+        {STEPS[currentStep].component}
       </div>
-    </div>
-  );
-}
+    );
+  }
 
