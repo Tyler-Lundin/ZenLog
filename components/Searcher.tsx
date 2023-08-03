da4a5424e759c4eb3c3965f929f54e988d9f8a04
Tyler Lundin <TylerJamesLundin@gmail.com>
2023-08-03 15:39:22 -0700

    Fixed overflowing / width issues

--- components/Searcher.tsx
+++ components/Searcher.tsx
@@ -9,11 +9,9 @@ export const Searcher = ({ search, setSearch }: { search: string, setSearch: (se
   }
 
   return (
-    <div className="grid items-center relative w-screen px-16">
-      <div className="relative bg-white dark:bg-black border rounded-full border-black/20 dark:border-white/20 border-black mx-auto grid w-full px-8">
-        <Input variant="glass" size="search" placeholder="incline bench press" className="text-center py-1 h-fit" type="search" value={search} onChange={handleSearchChange} />
-        <SearchIcon className="absolute top-1/2 right-4 transform  -translate-y-1/2 text-gray-500 dark:text-white" />
-      </div>
+    <div className="relative bg-white dark:bg-black border rounded-full border-black/20 dark:border-white/20 border-black mx-auto grid w-full px-8">
+      <Input variant="glass" size="search" placeholder="incline bench press" className="text-center py-1 h-fit w-full" type="search" value={search} onChange={handleSearchChange} />
+      <SearchIcon className="absolute top-1/2 right-4 transform  -translate-y-1/2 text-gray-500 dark:text-white" />
     </div>
   )
 }
