cba88660b5bace0f2a008b19f3c08504a6c3bc70
Tyler Lundin <TylerJamesLundin@gmail.com>
2023-08-03 04:38:40 -0700

    Initial UserGoals Hook / Component Created

--- components/dashboard/UserGoals.tsx
+++ components/dashboard/UserGoals.tsx
@@ -1,4 +1,62 @@
+import { WeightGoal, SleepGoal, ExerciseGoal, MeditateGoal } from "@prisma/client"
 
-export default function UserGoals() {
 
+// type WeightGoal {
+//   target Int
+// }
+//
+// type SleepGoal {
+//   totalSleep Int
+// }
+//
+// type ExerciseGoal {
+//   totalReps    Int
+//   totalEntries Int
+//   totalVolume  Int
+//   totalWeight  Int
+// }
+//
+// type MeditateGoal {
+//   totalTime Int
+// }
+//
+// type WaterGoal {
+//   totalOunces  Int
+//   totalCups    Int
+//   totalGallons Int
+// }
+
+export function useUserGoals() {
+
+  const weightGoal: WeightGoal = {
+    target: 185
+  }
+  const sleepGoal: SleepGoal = {
+    totalSleep: 8
+  }
+  const exerciseGoal: ExerciseGoal = {
+    totalReps: 100,
+    totalEntries: 10,
+    totalVolume: 1000,
+    totalWeight: 1000
+  }
+  const meditateGoal: MeditateGoal = {
+    totalTime: 10
+  }
+
+  return {
+    weightGoal,
+    sleepGoal,
+    exerciseGoal,
+    meditateGoal,
+  }
+}
+
+
+export default function UpdateUserGoals() {
+  return (
+    <form>
+      UPDATE USER GOALS HERE
+    </form>
+  )
 }
