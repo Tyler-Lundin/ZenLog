fb897ff22691e02b37636465ef8a55729067ce97
Tyler Lundin <TylerJamesLundin@gmail.com>
2023-08-03 04:33:45 -0700

    Updated Log Exercise Route Validation / Payloads

--- app/api/log/exercise/route.ts
+++ app/api/log/exercise/route.ts
@@ -1,10 +1,7 @@
-// import { ExerciseSet } from "@prisma/client";
+import { NewEntry } from '@/_store/slices/exerciseSlice'
 import useServerSession from '@/hooks/useServerSession'
 import { ExerciseEntry } from '@prisma/client'
-import { authOptions } from '@server/authOptions'
 import { prisma } from '@server/db'
-import { NextApiRequest, NextApiResponse } from 'next'
-import { getServerSession } from 'next-auth'
 import { NextRequest, NextResponse } from 'next/server'
 
 export type ExerciseSet = {
@@ -32,35 +29,19 @@ const convertToNumber = (set: ExerciseSet, field: ExerciseNumericField) => {
 }
 
 const validateRequestData = (
-  data: Omit<ExerciseEntry, 'userId' | 'updatedAt' | 'createdAt' | 'id'>
+  data: { newExercise: NewEntry, userDayId: string }
 ) => {
-  const { exerciseId, exerciseName, set, userDayId } = data
+  const { newExercise, userDayId } = data
+  const { exerciseId, exerciseName, weight, reps, intensity, toFailure } = newExercise
 
   // Basic field validation
   if (!exerciseId) throw new Error('Missing exerciseId')
   if (!exerciseName) throw new Error('Missing exerciseName')
-  if (!set) throw new Error('Missing set')
+  if (!weight) throw new Error('Missing weight')
+  if (!reps) throw new Error('Missing reps')
   if (!userDayId) throw new Error('Missing userDayId')
   if (typeof exerciseId !== 'string') throw new Error('Invalid exerciseId')
   if (typeof exerciseName !== 'string') throw new Error('Invalid exerciseName')
-
-    // Validate set
-
-    ;['reps', 'weight', 'intensity'].forEach((field) => {
-      convertToNumber(set, field as ExerciseNumericField)
-    })
-
-  if (
-    typeof set.toFailure !== 'boolean' ||
-    (set.notes && typeof set.notes !== 'string') ||
-    !Array.isArray(set.tags)
-  ) {
-    throw new Error(`Invalid set`)
-  }
-
-  set.tags.forEach((tag, j) => {
-    if (tag && typeof tag !== 'string') throw new Error(`Invalid tag at index ${j} in set`)
-  })
 }
 
 export async function POST(req: NextRequest, res: NextResponse) {
@@ -69,13 +50,11 @@ export async function POST(req: NextRequest, res: NextResponse) {
     if (!session) return NextResponse.json({ error: 'Not Authorized' })
 
     const { user: { id } } = session
-    const { newExercise, userDayId } = await req.json()
-    const { exerciseId, exerciseName, set } = newExercise as Omit<
-      ExerciseEntry,
-      'userId' | 'updatedAt' | 'createdAt' | 'userDayId'
-    >
+    const data = await req.json()
+    const { newExercise, userDayId } = data
+    const { exerciseId, exerciseName, weight, reps, toFailure, intensity, notes, tags } = newExercise as NewEntry
 
-    validateRequestData({ exerciseId, exerciseName, set, userDayId })
+    validateRequestData(data)
 
     const validateDate = await prisma.userDay.findUnique({
       where: {
@@ -92,13 +71,13 @@ export async function POST(req: NextRequest, res: NextResponse) {
         exerciseId,
         exerciseName,
         set: {
-          reps: set.reps,
-          weight: set.weight,
-          weightUnit: set.weightUnit || 'lb',
-          intensity: set.intensity,
-          toFailure: set.toFailure,
-          notes: set.notes || '',
-          tags: set.tags || [],
+          reps: reps,
+          weight: weight,
+          weightUnit: 'POUND',
+          intensity: intensity,
+          toFailure: toFailure,
+          notes: notes || '',
+          tags: tags || [],
         },
         userDayId,
         userId: id,
@@ -118,7 +97,6 @@ export async function POST(req: NextRequest, res: NextResponse) {
 
     return NextResponse.json({ exerciseEntry: loggedExercise }, { status: 201 })
   } catch (error: any) {
-    console.error(error)
     return NextResponse.json({ error: error.message || 'Something went wrong' })
   }
 }
