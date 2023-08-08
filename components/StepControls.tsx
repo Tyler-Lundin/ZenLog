c121ae4875e842018187fc6b7dd30c0e61e0d251
Tyler Lundin <TylerJamesLundin@gmail.com>
2023-08-08 06:30:39 -0700

    Cleaned up StepControls

--- components/StepControls.tsx
+++ components/StepControls.tsx
@@ -11,7 +11,6 @@ export default function StepControls({
   prevStep,
   className = "flex absolute w-full h-full top-0 left-0 items-center justify-between z-20 p-2 pointer-events-none",
   children,
-  type = "default"
 }: {
   isFirstStep: boolean,
   isLastStep: boolean,
@@ -20,21 +19,7 @@ export default function StepControls({
   prevStep: () => void
   className?: string
   children?: React.ReactNode
-  type?: "default" | "done"
 }) {
-
-  if (type === "done")
-    return (
-      <div className={className}>
-        <Button variant={isFirstStep ? "glassRed" : "glass"} size="xlSquare" className="rounded-full pointer-events-auto" onClick={isFirstStep ? handleClose : prevStep}>
-          {isFirstStep ? <AiOutlineClose /> : <AiOutlineLeft />}
-        </Button>
-        {children}
-        <Button variant={isLastStep ? "glassGreen" : "glass"} size="xlSquare" className="rounded-full pointer-events-auto" onClick={nextStep}>
-          {isLastStep ? <IoCheckmark /> : <AiOutlineRight />}
-        </Button>
-      </div>
-    )
 
   return (
     <div className={className}>
